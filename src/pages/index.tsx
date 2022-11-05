a4bfc75e9d928b70df589534dc8a86ac8864f7b4
Matheus Carvalho <matheus12bh@gmail.com>
2022-11-04 20:27:39 -0700

    add-axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { FormEvent, useState } from 'react';
+
 import Image from 'next/image'
 
 import appPreviewImg from '../assets/app-nlw-copa-preview.png';
@@ -6,7 +8,58 @@ import usersAvatarExampleImg from '../assets/users-avatar-example.png';
 
 import iconCheckImg from '../assets/icon-check.svg';
 
-export default function Home() {
+import { api } from '../lib/axios';
+
+interface HomeProps {
+  poolCount: number
+  guessCount: number
+  userCount: number
+}
+
+export const getServerSideProps = async () => {
+  const [
+    poolCountResponse,  
+    guessCountResponse, 
+    userCountReponse
+  ] = await Promise.all([
+    api.get('pools/count'),
+    api.get('guesses/count'),
+    api.get('users/count'),
+  ])
+
+  return {
+    props: {
+      poolCount: poolCountResponse.data.count,
+      guessCount: guessCountResponse.data.count,
+      userCount: userCountReponse.data.count,
+    }
+  }
+}
+
+export default function Home(props: HomeProps) {
+  const [poolTitle, setPoolTitle] = useState('');
+
+  async function createPool(event: FormEvent) {
+    event.preventDefault()
+
+    try {
+      const response = await api.post('pools', {
+        title: poolTitle
+      })
+
+      const { code } = response.data
+
+      navigator.clipboard.writeText(code)
+
+      alert('Bolão criado com successo! O código copiado para a área de transferência.')
+
+      setPoolTitle('')
+    }catch(err) {
+      alert('Falha ao criar bolão, tente novamente.')
+    }
+  }
+
+
   return (
     <div className="max-w-[1124px] h-screen mx-auto grid grid-cols-2 items-center gap-28">
       <main>
@@ -17,15 +70,17 @@ export default function Home() {
         <div className="mt-10 flex items-center gap-2">
           <Image src={usersAvatarExampleImg} alt="Avatars" />
           <strong className="text-gray-100 text-xl">
-            <span className="text-ignite-500">+12.592</span> Pessoas já estão usando
+            <span className="text-ignite-500">+{props.userCount}</span> Pessoas já estão usando
           </strong>
         </div>
-        <form className="mt-10 flex gap-2">
+        <form onSubmit={createPool} className="mt-10 flex gap-2">
           <input 
             className="flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm outline-none text-gray-300"
             type="text" 
             required 
             placeholder="Qual nome do seu bolão"
+            value={poolTitle}
+            onChange={event => setPoolTitle(event.target.value)}
            />
           <button 
             className="bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700"
@@ -39,7 +94,7 @@ export default function Home() {
           <div className="flex items-center gap-6">
               <Image src={iconCheckImg} alt="Icon Check" />
               <div className="flex flex-col">
-                <span className="font-bold text-2xl">+2.034</span>
+                <span className="font-bold text-2xl">+{props.poolCount}</span>
                 <span>Bolões criados</span>
               </div>
           </div>
@@ -47,7 +102,7 @@ export default function Home() {
           <div className="flex items-center gap-6">
               <Image src={iconCheckImg} alt="Icon Check" />
               <div className="flex flex-col">
-                <span className="font-bold text-2xl">+192.847</span>
+                <span className="font-bold text-2xl">+{props.guessCount}</span>
                 <span>Palpites enviados</span>
               </div>
           </div>
